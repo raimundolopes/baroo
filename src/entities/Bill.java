27e9e6469efb9bf4981cdaba792d2fb92cddc338
Raimundo Pereira <raimundo_lopes@uol.com.br>
2025-05-22 16:22:36 -0300

    adiciona classe esqueleto

---
+++ src/entities/Bill.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Bill {
+
+	public char gender;
+	public int beer;
+	public int barbecue;
+	public int softDrink;
+
+	public double cover() {
+
+		return 0.0;
+
+	}
+
+	public double feeding() {
+
+		return 0.0;
+
+	}
+
+	public double ticket() {
+
+		return 0.0;
+
+	}
+
+	public double total() {
+
+		return 0.0;
+
+	}
+
+}
